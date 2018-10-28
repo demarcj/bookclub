6b768149ee1de99835bf4afdab51c6561f6c7555
DeMarc Johnson <demarcjohnson@gmail.com>
2018-10-28 19:10:44 -0400

    The added a button and took the code out of genre into the script file because the random method would not randomize more than once

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
-import genreList from './genre.js';
-document.getElementById("genre").innerHTML = genreList;
+addEventListener('click', function(){
+  const genreList = [`Action/Shonen`, `Drama/Romance`, `Comedy`, `Mystery/Thriller`, `Sci-Fi/Mecha`, `Horror/Psychological`, `Moe/Slice of Life`, `Fantasy/Isekai`, `Sports/Game`];
+  const getGenre = ()=> Math.floor(Math.random() * genreList.length);
+  document.getElementById("genre").innerHTML = genreList[getGenre()];
+})
