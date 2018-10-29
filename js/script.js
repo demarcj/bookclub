b28f7ce042a253e70b2c1ad0aab671a7af7731e5
DeMarc Johnson <demarcjohnson@gmail.com>
2018-10-28 21:08:34 -0400

    I've add a show all option for the genre and added the a button to hide the genre list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-addEventListener('click', function(){
-  const genreList = [`Action/Shonen`, `Drama/Romance`, `Comedy`, `Mystery/Thriller`, `Sci-Fi/Mecha`, `Horror/Psychological`, `Moe/Slice of Life`, `Fantasy/Isekai`, `Sports/Game`];
-  const getGenre = ()=> Math.floor(Math.random() * genreList.length);
-  document.getElementById("genre").innerHTML = genreList[getGenre()];
-})
+const genreList = [`Action/Shonen`, `Drama/Romance`, `Comedy`, `Mystery/Thriller`, `Sci-Fi/Mecha`, `Horror/Psychological`, `Moe/Slice of Life`, `Fantasy/Isekai`, `Sports/Game`];
+const doc = (str, html)=>{document.getElementById(str).innerHTML = html;};
+const el = (str)=>document.getElementById(str); 
+
+const getGenre = ()=> {
+  const randomize = ()=> Math.floor(Math.random() * genreList.length);
+  doc("genre", genreList[randomize()]);
+}
+
+const getAllGenre = ()=> {
+  const genreMap = el("allGenre").innerHTML === "" ? genreList.join(" <br>") : "";
+  const buttonText = genreMap === "" ? "Show All" : "Hide";
+  doc("showAllGenre", buttonText);
+  doc("allGenre", genreMap);
+};
+el("generate").addEventListener('click', getGenre);
+el("showAllGenre").addEventListener('click', getAllGenre);
